1ce718b0f405b4ee5b8a6ea32cad6d9031444df0
Svetlana <isaeva322@mail.ru>
2022-11-14 21:14:41 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println(" Задание 1");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println( " Поздравляем Вас с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println( " Возраст совешеннолетия еще не наступил, Вам нужно немного подождать!");}
+
     }
 }
